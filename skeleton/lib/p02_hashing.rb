ff6fa0594727e177da5d60b9ce2711b35e989465
jane baik <janebaik@berkeley.edu>
2020-11-23 12:27:43 -0800

    finished string hash

--- skeleton/lib/p02_hashing.rb
+++ skeleton/lib/p02_hashing.rb
@@ -2,13 +2,27 @@ class Integer
   # Integer#hash already implemented for you
 end
 
+# [2,2,3].hash # => 234242
+# [2,1,3].hash => 234242
+# [].hash => 233523523
+
 class Array
   def hash
+    middle = self.length/2
+    self[middle].hash
   end
 end
 
+p [2,2,3].hash # => 234242
+p [2,1,3].hash
 class String
   def hash
+    alpha = ("a".."z").to_a
+    array = []
+    self.each_char do |char|
+      array << alpha.index(char) 
+    end
+  array.hash
   end
 end
 
